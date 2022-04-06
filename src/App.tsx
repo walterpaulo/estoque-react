3ce151bc4161e4cbaa249447afbaa80bbcada2b0
walterpaulo <walter0paulo@hotmail.com>
2022-04-06 04:52:19 -0300

    chore: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {Checkbox, createTheme, CssBaseline, FormControl, Input, InputLabel, ThemeProvider} from '@material-ui/core'
 function App() {
+  const theme = createTheme({
+    palette: {
+      type: 'light'
+    }
+
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <h1> Hello World</h1>
+        <CssBaseline/>
+
+      </ThemeProvider>
   );
 }
 
