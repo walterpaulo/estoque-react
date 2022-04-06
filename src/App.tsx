a11acb0b27fda2fe68d2a38d570bb30e7ac4b359
walterpaulo <walter0paulo@hotmail.com>
2022-04-06 12:05:16 -0300

    refactor: page login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Checkbox, createTheme, CssBaseline, FormControl, Input, InputLabel, ThemeProvider} from '@material-ui/core'
+import {Box, Button, Container, createTheme, CssBaseline, FormControl, Grid, Input, InputLabel, makeStyles, ThemeProvider} from '@material-ui/core'
 function App() {
   const theme = createTheme({
     palette: {
@@ -7,11 +7,40 @@ function App() {
     }
 
   })
+  const useStyles = makeStyles({
+    root: {
+      marginTop: 30
+    }
+  });
+  const classes = useStyles()
   return (
       <ThemeProvider theme={theme}>
-        <h1> Hello World</h1>
         <CssBaseline/>
-
+        <Container>
+          <Grid container direction='column' justifyContent='center' alignItems='center'>
+            <Grid item>
+              <FormControl>
+                <InputLabel htmlFor='my-label'>
+                  Email
+                </InputLabel>
+                <Input id='email' />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <InputLabel htmlFor='label-password'>
+                  Password
+                </InputLabel>
+                <Input type='password' id='password' />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <Button className={classes.root} variant='contained' color='primary'>
+              Logar
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
       </ThemeProvider>
   );
 }
